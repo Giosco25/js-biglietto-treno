533299088850bdc3c62d6dc4d87d2bccc42ee494
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-04-20 16:34:34 +0200

    modifiche

--- myscript.js
+++ myscript.js
@@ -7,23 +7,23 @@ console.log(chilometri);
 //calcolo prezzo totale viaggio
 var calcolo = chilometri * 0.21;
 console.log(calcolo);
-// if(anni_utente <= 17){
-//     console.log(calcolo / 20%);
-// } else (anni_utente >= 65){
-//     console.log(calcolo / 40%);
-// }
 //applico sconto minorenni
 var sconto_minorenni = calcolo * 20 / 100;
 console.log(sconto_minorenni);
 //applico sconto over 65
 var sconto_over65 = calcolo * 40 / 100;
 console.log(sconto_over65);
-
 if (anni_utente < 18) {
     console.log(sconto_minorenni);
+    document.getElementById('prezzo').innerHTML = 'Il prezzo del biglietto è di' + sconto_minorenni;
 }else if (anni_utente > 65) {
     console.log(sconto_over65);
+    document.getElementById('prezzo').innerHTML = 'Il prezzo del biglietto è di' + sconto_over65;
 }
 else {
     console.log(calcolo);
+    document.getElementById('prezzo').innerHTML = 'Il prezzo del biglietto è di' + calcolo;
 }
+document.getElementById('title').innerHTML = 'Benvenuto/a';
+
+//&nbsp
